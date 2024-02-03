f99480fa2b5fcf6432f317a08326a4e95ca5f23a
ElStron <luisstron45@gmail.com>
2024-02-03 01:24:14 -0600

    add api urls

--- src/constants.ts
+++ src/constants.ts
@@ -3,5 +3,7 @@ export const API_URLS = {
     'ITEMS': 'https://fortniteapi.io/v2/items/list?lang=en',
     'BUNDLES': 'https://fortniteapi.io/v2/bundles/list?lang=en',
     'ITEM_DETAILS': ' https://fortniteapi.io/v2/items/get?id=',
-    'CREATIVE' : 'https://fortniteapi.io/v3/creative/discovery'
+    'CREATIVE' : 'https://fortniteapi.io/v3/creative/discovery',
+    'ISLAND': 'https://fortniteapi.io/v1/creative/island?code=',
+    'FEATURED': 'https://fortniteapi.io/v1/creative/featured'
 }
