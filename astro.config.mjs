7ffb9b650f31b15a1b3de470a745e3b7053b6914
ElStron <luisstron45@gmail.com>
2024-02-05 23:57:07 -0600

    fix adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import cloudflare from "@astrojs/cloudflare";
-import vercel from "@astrojs/vercel";
-
-const adapter = import.meta.env.API_KEY === 'vercel' ? vercel() : cloudflare({ mode: 'directory' });
 
 export default defineConfig({
   integrations: [tailwind({
     applyBaseStyles: true
   })],
   output: "server",
-  adapter: adapter
+  adapter: cloudflare({ mode: 'directory' })
 });
