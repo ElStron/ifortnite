de4d0ce049d75d94e1bc6e1bb961209bc5a92ee2
ElStron <luisstron45@gmail.com>
2024-02-05 23:51:02 -0600

    remove pre-render

--- src/pages/api/update.json.ts
+++ src/pages/api/update.json.ts
@@ -1,5 +1,4 @@
 import type { APIRoute } from 'astro';
-export const prerender = true;
 
 export const POST: APIRoute = async ({ request }) => {
     const response = await fetch( 'https://api.vercel.com/v1/integrations/deploy/prj_xGNgyuZ9nCEW9YzRZTH7f5aepPjz/NihVGOB8wY' );  
