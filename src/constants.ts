b9cce3fa63b684b641c440c4d4760c2e333d0071
ElStron <luisstron45@gmail.com>
2024-02-06 01:07:45 -0600

    fix url items details

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ export const API_URLS = {
     'SHOP': 'https://fortniteapi.io/v2/shop?lang=en',
     'ITEMS': 'https://fortniteapi.io/v2/items/list?lang=en',
     'BUNDLES': 'https://fortniteapi.io/v2/bundles/list?lang=en',
-    'ITEM_DETAILS': ' https://fortniteapi.io/v2/items/get?id=',
+    'ITEM_DETAILS': 'https://fortniteapi.io/v2/items/get?id=',
     'CREATIVE' : 'https://fortniteapi.io/v3/creative/discovery',
     'ISLAND': 'https://fortniteapi.io/v1/creative/island?code=',
     'FEATURED': 'https://fortniteapi.io/v1/creative/featured'
