c2e0649a50816bb834d43f12d2abd07a7f7daddc
ElStron <luisstron45@gmail.com>
2024-02-08 23:44:58 -0600

    Creative endpoint has been added

--- src/services/endpoints.ts
+++ src/services/endpoints.ts
@@ -2,6 +2,7 @@ export const ENDPOINTS = {
     'v1': 'https://fortniteapi.io/v1',
     'v2': 'https://fortniteapi.io/v2',
     'v3': 'https://fortniteapi.io/v3',
+    'v4': 'https://race.f1calendar.live/',
 }
 
 
@@ -13,6 +14,6 @@ export const bundlesList = ( { lang }:{ lang: string }) => `${ENDPOINTS.v2}/bund
 
 export const itemDetails = ( { id, lang }:{ id: string, lang: string }) => `${ENDPOINTS.v2}/items/get?id=${id}&lang=${lang}`
 
-export const creativeList = ( { lang }:{ lang: string }) => `${ENDPOINTS.v3}/creative/discovery?lang=${lang}`
+export const creativeList = ( { lang }:{ lang: string }) => `${ENDPOINTS.v4}/creative/?lang=${lang}`
 
 export const islandDetails = ( { code, lang }:{ code: string, lang: string }) => `${ENDPOINTS.v1}/creative/island?code=${code}&lang=${lang}`
