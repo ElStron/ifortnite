b1593e4698a78346e0071368a497b46b2255ecce
ElStron <luisstron45@gmail.com>
2024-02-12 21:01:46 -0600

    Add Preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,21 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import cloudflare from "@astrojs/cloudflare";
+import preact from "@astrojs/preact";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://ifornite.com',
   integrations: [tailwind({
     applyBaseStyles: true
-  })],
+  }), preact()],
   output: "server",
-  adapter: cloudflare({ mode: "directory" }),
+  adapter: cloudflare({
+    mode: "directory"
+  }),
   i18n: {
     defaultLocale: "en",
-    locales: ["en","es"],
+    locales: ["en", "es"],
     routing: {
       prefixDefaultLocale: false
     }
