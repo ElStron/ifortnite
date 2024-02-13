65bcb3666b837903efebf111fe634dbe376c1e15
ElStron <luisstron45@gmail.com>
2024-02-12 21:07:51 -0600

    Add pagination Creative list

--- src/services/creative.ts
+++ src/services/creative.ts
@@ -1,11 +1,25 @@
 import { creativeList } from "./endpoints"
-import { fetchData } from "./fetch"
+import { fetchItems } from "./fetch"
+
+
+const pagination = (data: any, page: number, itemsPerPage: number) => {
+    const itemsNumber = itemsPerPage ? itemsPerPage : 10
+    const items = data.slice(page * itemsNumber, (page + 1) * itemsNumber)
+    return items
+}
 
 export const getCreative = async ( { 
     lang = 'en',
+    page,
+    itemsPerPage
 }:{ 
-    lang: string | undefined, 
+    lang: string | undefined,
+    page?: number | undefined
+    itemsPerPage?: number | undefined 
 }) => {
-    const data = await fetchData(creativeList({ lang: lang }))
+    let data = await fetchItems(creativeList({ lang: lang }))
+    if (page) {
+        data = pagination(data, page, itemsPerPage)
+    }
     return data
 }
