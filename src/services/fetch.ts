82a4f54e7dd6f13a718e25fa1eba658d5c1e5b38
ElStron <luisstron45@gmail.com>
2024-02-12 21:08:07 -0600

    export fetch

--- src/services/fetch.ts
+++ src/services/fetch.ts
@@ -1,11 +1,23 @@
 const API_KEY = import.meta.env.API_KEY
 
+
 export async function fetchData(url: string) {
     const response = await fetch(url, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
             'Authorization': API_KEY
+        }
+    });
+    const data = await response.json();
+    return data
+}
+
+export async function fetchItems(url: string) {
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
         }
     });
     const data = await response.json();
