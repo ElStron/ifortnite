6ba0b2cdd6d51035734c9431353085a2cf90b242
ElStron <luisstron45@gmail.com>
2024-02-27 00:36:27 -0600

    Update callback URL

--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -16,7 +16,9 @@ export const POST: APIRoute = async ({ request, cookies, redirect }) => {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: provider as Provider,
       options: {
-        redirectTo: import.meta.env.CB_URL || "http://localhost:4321/api/auth/callback",
+        redirectTo: import.meta.env.CB_URL 
+          ? import.meta.env.CB_URL
+          : "http://localhost:4321/api/auth/callback"
       },
     });
 
