bb84d10039cb8c01dec0ae4a22f437dde02bf53b
ElStron <luisstron45@gmail.com>
2024-02-27 00:54:11 -0600

    add providers

--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -8,7 +8,7 @@ export const POST: APIRoute = async ({ request, cookies, redirect }) => {
   const password = formData.get("password")?.toString();
   const provider = formData.get("provider")?.toString();
 
-  const validProviders = ["google", "github", "discord"];
+  const validProviders = ["google", "twitter", "discord"];
 
   if (provider && validProviders.includes(provider)) {
 
