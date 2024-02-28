c856204f8820d714e8b1bbbb0ac72dc1d08c0445
ElStron <luisstron45@gmail.com>
2024-02-27 20:23:42 -0600

    Fix build format

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,7 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ifornite.com',
-  trailingSlash: 'never',
   build: {
-    format: 'file',
     assets: '_assets',
   },
   integrations: [tailwind({
