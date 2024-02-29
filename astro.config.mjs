4c1f7a00928fc4299745ce1f89a97777abeaebd6
ElStron <luisstron45@gmail.com>
2024-02-28 22:58:17 -0600

    Update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ifornite.com',
-  trailingSlash: 'never',
   build: {
     assets: '_assets',
   },
