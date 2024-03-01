c35faffbd1bcfcd65d924e39b11e2746e0601ec5
Stron <luisstron45@gmail.com>
2024-02-29 21:37:20 -0600

    Update update.json.ts
    Update

--- src/pages/api/update.json.ts
+++ src/pages/api/update.json.ts
@@ -1,7 +1,7 @@
 import type { APIRoute } from 'astro';
 
 export const POST: APIRoute = async ({ request }) => {
-    const response = await fetch( 'https://api.vercel.com/v1/integrations/deploy/prj_xGNgyuZ9nCEW9YzRZTH7f5aepPjz/NihVGOB8wY' );  
+    const response = await fetch( '...' );  
     const data = await response.json();
     return new Response(JSON.stringify(data)
     )
