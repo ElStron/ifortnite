4925e5edd7809295d8ef95b3de2a6e6201df3b2d
ElStron <luisstron45@gmail.com>
2024-03-05 01:03:49 -0600

    [ Add ] FilterItemBySection function has been added.

--- src/services/dailyShop.ts
+++ src/services/dailyShop.ts
@@ -1,14 +1,47 @@
 import { dailyShop } from "./endpoints"
 import { fetchData } from "./fetch"
 
+export function filterItemsBySection(items: any) {
+    const sections: { id: string; name: string; price: number; items?: any[] }[] = [];
+
+    for (const item of items) {
+        const exists = sections.find((section) => section.id === item.section.id);
+        if (!exists) {
+            const price = item.price.regularPrice - item.price.finalPrice;
+            sections.push({ id: item.section.id, name: item.section.name, price: price });
+        }
+    }
+
+    sections.sort((a: any, b: any) => {
+        const numA = a.price;
+        const numB = b.price;
+
+        return numB - numA;
+    });
+
+    sections.forEach((section) => {
+        section.items = items.filter((item: any) => item.section.id === section.id);
+    });
+
+    return sections;
+}
+
+
 export const getDailyShop = async ( { 
     lang = 'en',
-    type = undefined  
+    type = undefined,
+    filter  
 }:{ 
     lang: string | undefined, 
-    type?: string | undefined 
+    type?: string | undefined
+    filter?: string 
 
 }) => {
-    const data = await fetchData(dailyShop({ lang: lang }))
+    let data = await fetchData(dailyShop({ lang: lang }))
+    
+    if (filter === 'section') {
+        return data.shop = filterItemsBySection(data.shop)
+    }
+    
     return data
 }
