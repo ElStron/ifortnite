a7352ef460b26da8d535711f4c111686cd1ccdb8
ElStron <luisstron45@gmail.com>
2024-03-05 23:01:39 -0600

    Remove Api urls & add hover constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,3 @@
-export const API_URLS = {
-    'SHOP': 'https://fortniteapi.io/v2/shop',
-    'ITEMS': 'https://fortniteapi.io/v2/items/list',
-    'BUNDLES': 'https://fortniteapi.io/v2/bundles/list?lang=en',
-    'ITEM_DETAILS': 'https://fortniteapi.io/v2/items/get?id=',
-    'CREATIVE' : 'https://fortniteapi.io/v3/creative/discovery',
-    'ISLAND': 'https://fortniteapi.io/v1/creative/island?code=',
-    'FEATURED': 'https://fortniteapi.io/v1/creative/featured'
-}
+export const HOVER = 'hover:scale-105 hover:ring-2 hover:ring-white transition-all'
 
 export const URL_IMG = (id: string) => `https://fortnite-api.com/images/cosmetics/br/materialinstances/${id}/background_v1-2.webp`;
