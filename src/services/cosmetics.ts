3229ea4701d468a8103fda05b3c88441528ba184
ElStron <luisstron45@gmail.com>
2024-03-11 19:36:16 -0600

    Remove filterItemsBySection on getCosmetics

--- src/services/cosmetics.ts
+++ src/services/cosmetics.ts
@@ -1,7 +1,7 @@
 import { itemList, itemDetails } from "./endpoints"
 import { fetchData } from "./fetch"
 
-const filterItemsByType =  (type: string, items: any) => {
+export const filterItemsByType =  (type: string, items: any) => {
     const filteredItems = items.filter((item: any) => item.type.id === type || item.upcoming === type)
     return filteredItems
 }
@@ -9,16 +9,12 @@ const filterItemsByType =  (type: string, items: any) => {
 
 export const getCosmetics = async ( { 
     lang = 'en',
-    type = undefined  
 }:{ 
     lang: string | undefined, 
-    type?: string | undefined 
 
 }) => {
     const data = await fetchData(itemList({ lang: lang }))
     const items = data.items
-
-    if (type) return filterItemsByType(type, items)
 
     return items
 }
