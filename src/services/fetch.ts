48fea2ab046facb5c5cd2e0b5a748d457704e550
ElStron <luisstron45@gmail.com>
2024-03-11 19:42:37 -0600

    Error handling in fetch

--- src/services/fetch.ts
+++ src/services/fetch.ts
@@ -2,15 +2,25 @@ const API_KEY = import.meta.env.API_KEY
 
 
 export async function fetchData(url: string) {
-    const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': API_KEY
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': API_KEY
+            }
+        })
+        if (!response.ok) {
+            console.log(response)
+            throw new Error('Failed to fetch data')
         }
-    });
-    const data = await response.json();
-    return data
+
+        const data = await response.json()
+        return data
+    }
+    catch (error) {
+        console.log(error)
+    }
 }
 
 export async function fetchItems(url: string) {
