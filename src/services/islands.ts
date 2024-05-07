5df4ef6e2fe838bc74232a2db9aa875fa6ac1dc3
ElStron <luisstron45@gmail.com>
2024-05-07 16:27:45 -0600

    add service, player count

--- src/services/islands.ts
+++ src/services/islands.ts
@@ -1,4 +1,4 @@
-import { islandDetails } from "./endpoints";
+import { islandDetails, playerCount } from "./endpoints";
 import { fetchData } from "./fetch";
 
 export const getIsland = async ( { 
@@ -12,4 +12,24 @@ export const getIsland = async ( {
     const response = await fetchData( islandDetails({ code, lang }) );
     return response
 
+}
+
+const getPlayerCountByLink = (data: any, link: string) => {
+    // Use the `find` method to find the matching island
+    const matchingIsland = data.discovery.flatMap((section: any) => section.islands)
+      .find((island: any) => island.link === link);
+  
+    // Return the player count or -1 if not found
+    return matchingIsland ? matchingIsland.globalPlayerCount : 0;
+  };
+  
+
+export const playerCountByCode = async ( {
+    code,
+}:{
+    code: string,
+} ) =>  {
+    const response = await fetchData( playerCount() )
+    const players = getPlayerCountByLink(response, code)
+    return players
 }
