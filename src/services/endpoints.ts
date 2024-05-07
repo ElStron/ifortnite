8dce6d03d406550680d004e63803301d3332ec1d
ElStron <luisstron45@gmail.com>
2024-05-07 16:27:29 -0600

    add endpoint (player count)

--- src/services/endpoints.ts
+++ src/services/endpoints.ts
@@ -14,6 +14,8 @@ export const bundlesList = ( { lang }:{ lang: string }) => `${ENDPOINTS.v2}/bund
 
 export const itemDetails = ( { id, lang }:{ id: string, lang: string }) => `${ENDPOINTS.v2}/items/get?id=${id}&lang=${lang}`
 
+export const playerCount = () => `${ENDPOINTS.v3}/creative/discovery`
+
 export const creativeList = ( { lang }:{ lang: string }) => `${ENDPOINTS.v4}/creative?lang=${lang}`
 
 export const islandDetails = ( { code, lang }:{ code: string, lang: string }) => `${ENDPOINTS.v1}/creative/island?code=${code}&lang=${lang}`
